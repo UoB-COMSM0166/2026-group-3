4aa991b0587dfffa7e4704f0e22ee6fb833f63d4
Matthew-T-Honey <matthewthoney@gmail.com>
2026-01-31 14:10:28 +0000

    Added Alpha Slider

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,6 +1,7 @@
+let Rslider;
 let Gslider;
 let Bslider;
-let Rslider;
+let Aslider;
 
 function setup() {
     createCanvas(700, 700);
@@ -10,19 +11,22 @@ function setup() {
 
     Rslider = createSlider(0,255);
     Rslider.position(20,100);
-    text("R",0,120);
+    text("R",0,115);
     Gslider = createSlider(0,255);
     Gslider.position(20,120);
-    text("G",0,140);
+    text("G",0,135);
     Bslider = createSlider(0,255);
     Bslider.position(20,140);
-    text("B",0,160);
+    text("B",0,155);
+    Aslider = createSlider(0,255);
+    Aslider.position(20,160);
+    text("A",0,175);
 }
 
 function draw() {
     fill(220);
-    rect(20,100,140,70);
-    fill(Rslider.value(),Gslider.value(),Bslider.value())
+    rect(20,100,140,80);
+    fill(Rslider.value(),Gslider.value(),Bslider.value(),Aslider.value())
     square(10,50,40);
 
     if (mouseIsPressed){
