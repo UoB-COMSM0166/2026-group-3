6254ba4f5ed1ce153916bb89b10dd08ea381e407
Matthew-T-Honey <matthewthoney@gmail.com>
2026-04-28 13:16:56 +0100

    fix

--- docs/src/Core/StoryData.js
+++ docs/src/Core/StoryData.js
@@ -1,6 +1,6 @@
 export const INTRO_STORY_DATA = [
     {
-        imagePath: "assets/story/scene0.png",
+        imagePath: "assets/story/Scene0.png",
         text: "I never asked to inherit this kitchen. But every time I look at my father's photo, I remember how it all began.",
         isBlackScreen: false
     },
@@ -10,32 +10,32 @@ export const INTRO_STORY_DATA = [
         isBlackScreen: true
     },
     {
-        imagePath: "assets/story/scene1.png",
+        imagePath: "assets/story/Scene1.png",
         text: "The cities fell first. Streets emptied, towers burned, and the old world began to disappear.",
         isBlackScreen: false
     },
     {
-        imagePath: "assets/story/scene2.png",
+        imagePath: "assets/story/Scene2.png",
         text: "After the Shamble Shift, plants died, animals vanished, and food became rarer than gold.",
         isBlackScreen: false
     },
     {
-        imagePath: "assets/story/scene3.png",
+        imagePath: "assets/story/Scene3.png",
         text: "My Dad faced a choice: keep his hands clean, or keep people alive.",
         isBlackScreen: false
     },
     {
-        imagePath: "assets/story/scene4.png",
+        imagePath: "assets/story/Scene4.png",
         text: "It was disgusting. It was dangerous. But somehow... it worked.",
         isBlackScreen: false
     },
     {
-        imagePath: "assets/story/scene5.png",
+        imagePath: "assets/story/Scene5.png",
         text: "From that day on, the Doomsday Kitchen was born.",
         isBlackScreen: false
     },
     {
-        imagePath: "assets/story/scene6.png",
+        imagePath: "assets/story/Scene6.png",
         text: "Forty years later, it is my turn to cook, defend, and survive.",
         isBlackScreen: false
     }
